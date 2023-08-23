211aab0e40b52453e63beb7181f52bcdb8e94f8c
andrewhenin <ahenin1@swarthmore.edu>
2023-08-22 20:42:02 -0700

    created app header

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="project-metadata">
+          <span className="hackathon-title">
+            7-Day Hackathon
+          </span>
+          <span className="project-number">
+            Project 2
+          </span>
+        </div>
+        <div className="project-title">
+          Tic Tac Toe
+        </div>
       </header>
     </div>
   );
